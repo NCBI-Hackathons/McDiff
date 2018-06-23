a75602dc4fcd4c75cec5618876839e4ca6d95d46
Jonathan DeMasi <jode5573@colorado.edu>
2018-06-23 13:52:49 -0600

    form updates

--- app/forms.py
+++ app/forms.py
@@ -6,10 +6,39 @@ from werkzeug import secure_filename
 
 class FaddForm(FlaskForm):
 
+    # CSV File
     csv = FileField(validators=[FileRequired(), FileAllowed(['csv'])])
+
+    # File 2 
+    roi_file = FileField(validators=[FileRequired(), FileAllowed(['txt'])])
+
+    # File 3 
+    mask_file = FileField(validators=[FileRequired(), FileAllowed(['txt'])])
+
+    # bound_d
+    bound_d = IntegerField('bound_d', validators=[DataRequired(message="This field is required and must be an int between 0 and 100"), NumberRange(min=1, max=100)], render_kw={"value": "20"})
+    
+    # exp_time
+    exp_time = FloatField('exp_time', validators=[DataRequired(message="This field is required and must be an int between 0 and 60"), NumberRange(min=0, max=15)])
+
+
+    # sigmaD
+    sigma_d = IntegerField('sigma_d', validators=[DataRequired(message="This field is required and must be an int between 0 and 100"), NumberRange(min=1, max=100)], render_kw={"value": "2"})
+
+
+    # sigmaF
+    sigma_f = FloatField('sigma_f', validators=[DataRequired(message="This field is required and must be an int between 0 and 60"), NumberRange(min=0, max=15)], render_kw={"value": ".05"})
+
+    
+    # mcm_temp
+    mcmc_temp = IntegerField('mcmc_temp', validators=[DataRequired(message="This field is required and must be an int between 0 and 100"), NumberRange(min=1, max=100)], render_kw={"value": "1"})
+
+
+    # Offset
     offset = FloatField('Offset', validators=[DataRequired(message="This field is required and must be an int between 0 and 60"), NumberRange(min=0, max=15)], render_kw={"value": "10.5"})
 
-    steps = IntegerField('Steps', validators=[DataRequired(message="This field is required and must be an int between 0 and 600"), NumberRange(min=1, max=600)], render_kw={"value": "60"})
+    # Steps
+    steps = IntegerField('Steps', validators=[DataRequired(message="This field is required and must be an int between 0 and 600"), NumberRange(min=1, max=600)], render_kw={"value": "200"})
 
     # Submit button
     submit = SubmitField('Run!', render_kw={"class": "btn btn-secondary"})
--- app/routes.py
+++ app/routes.py
@@ -2,20 +2,19 @@ from flask import render_template, redirect, url_for
 from app import app
 from app.forms import FaddForm
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     form = FaddForm()
-    if form.validate_on_submit():
-        return(redirect('/results/'))
-
-    # If we aren't processing our form, do this
     return(render_template('index.html', title='Home', form=form))
 
-@app.route('/results/')
-def results(val1, val2, val3, width):
-    expectation, lower, upper, ps, filename = wrapper(val1, val2, val3, width)
-    expectation = round(expectation, 1)
-    return(render_template('results.html', title='Results', value1=val1, value2=val2, value3=val3, width=width, expectation=expectation, filename="/static/img/{}".format(filename)))
+@app.route('/results/', methods=['GET', 'POST'])
+def results():
+    form = FaddForm()
+    if form.validate_on_submit():
+        pass
+    else:
+        return(redirect(url_for('index')))
+
 
 @app.route('/acknowledgements/')
 def acknowledgements():
