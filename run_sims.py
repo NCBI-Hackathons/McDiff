f3327a9c4403134444913214477f4f672ba99b4b
benja-matic <benjapaulcohen@gmail.com>
2018-06-23 14:58:29 -0400

    sketch code for compressed grid optimization

--- run_sims.py
+++ run_sims.py
@@ -29,31 +29,40 @@ np.random.seed(1200)
 
 #########################MCMC
 
-sigmaD = 2.
-sigmaF = .05
-N = 200
-OP, Error, AP, bool_flag_1, bool_flag_2, Iterate_ended = MCMC(20, .1, .5, nuc, roi, N, 1, sigmaD, sigmaF, 0, 1, 0, 20)
+# sigmaD = 2.
+# sigmaF = .05
+# N = 200
+# OP, Error, AP, bool_flag_1, bool_flag_2, Iterate_ended = MCMC(20, .1, .5, nuc, roi, N, 1, sigmaD, sigmaF, 0, 1, 0, 20)
+#
+# lo_mejor = Error.argmin() #index of parameter optimal
+# los_mejores = AP[:, lo_mejor] #best parameters
+#
+# stuck, roi_pre = simulate(los_mejores[0], los_mejores[1], 0.5, nuc, roi, sim_len)
+#
+# stuck_norm = stuck / roi_pre
+# stuck_time = np.arange(sim_len+1) * 0.18 #converts array indices into seconds
+#
+# fig, ax = plt.subplots(4)
+# ax[0].plot(stuck_time, stuck_norm, ".", label = "Simulation")
+# ax[0].plot(data[0,:], data_norm, ".", label = "Data")
+# ax[0].legend()
+# ax[0].set_xlabel("Time (s)")
+# ax[0].set_ylabel("Fraction of Proteins Bound/Baseline")
+# ax[1].hist(AP[0,:])
+# ax[2].hist(AP[1,:])
+# ax[3].plot(Error)
+# plt.show()
 
-lo_mejor = Error.argmin() #index of parameter optimal
-los_mejores = AP[:, lo_mejor] #best parameters
-
-stuck, roi_pre = simulate(los_mejores[0], los_mejores[1], 0.5, nuc, roi, sim_len)
-
-stuck_norm = stuck / roi_pre
-stuck_time = np.arange(sim_len+1) * 0.18 #converts array indices into seconds
-
-fig, ax = plt.subplots(4)
-ax[0].plot(stuck_time, stuck_norm, ".", label = "Simulation")
-ax[0].plot(data[0,:], data_norm, ".", label = "Data")
-ax[0].legend()
-ax[0].set_xlabel("Time (s)")
-ax[0].set_ylabel("Fraction of Proteins Bound/Baseline")
-ax[1].hist(AP[0,:])
-ax[2].hist(AP[1,:])
-ax[3].plot(Error)
-plt.show()
-
-
+f_bleached = .54
+N = 50
+L = 10
+s1 = .9
+s2 = .9
+fmin = 0
+fmax = 1
+dmin = 0
+dmax = 20
+results = CF(f_bleached, nuc, roi, N, fmin, fmax, dmin, dmax, s1, s2, N, L)
 
     # newfile = open("MCMC_results.txt", 'w')
     # for i in range(len(E)):
